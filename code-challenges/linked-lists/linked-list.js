bd40dac91b95d2c4109ed878f98d7e8c4c689b56
Alex2Pena <attn.Alex.P@gmail.com>
2020-04-20 15:28:04 -0700

    started code challenge

--- code-challenges/linked-lists/linked-list.js
+++ code-challenges/linked-lists/linked-list.js
@@ -23,3 +23,48 @@ Features:
 // Will return false when searching for a value in the linked list that does not exist
 // Can properly return a collection of all the values that exist in the linked list
 // Ensure your tests are passing before you submit your solution.
+
+
+
+// Code challenge 6
+
+// Feature Tasks
+// Write the following methods for the Linked List class:
+
+// .append(value) which adds a new node with the given value to the end of the list
+// .insertBefore(value, newVal) which add a new node with the given newValue immediately before the first value node
+// .insertAfter(value, newVal) which add a new node with the given newValue immediately after the first value node
+// Examples
+// .append(value)
+// Input	Args	Output
+// head -> [1] -> [3] -> [2] -> X	5	head -> [1] -> [3] -> [2] -> [5] -> X
+// head -> X	1	head -> [1] -> X
+// .insertBefore(value, newVal)
+// Input	Args	Output
+// head -> [1] -> [3] -> [2] -> X	3, 5	head -> [1] -> [5] -> [3] -> [2] -> X
+// head -> [1] -> [3] -> [2] -> X	1, 5	head -> [5] -> [1] -> [3] -> [2] -> X
+// head -> [1] -> [2] -> [2] -> X	2, 5	head -> [1] -> [5] -> [2] -> [2] -> X
+// head -> [1] -> [3] -> [2] -> X	4, 5	Exception
+// .insertAfter(value, newVal)
+// Input	Args	Output
+// head -> [1] -> [3] -> [2] -> X	3, 5	head -> [1] -> [3] -> [5] -> [2] -> X
+// head -> [1] -> [3] -> [2] -> X	2, 5	head -> [1] -> [3] -> [2] -> [5] -> X
+// head -> [1] -> [2] -> [2] -> X	2, 5	head -> [1] -> [2] -> [5] -> [2] -> X
+// head -> [1] -> [3] -> [2] -> X	4, 5	Exception
+// Unit Tests
+// Utilize the Single-responsibility principle: any methods you write should be clean, reusable, abstract component parts to the whole challenge. You will be given feedback and marked down if you attempt to define a large, complex algorithm in one function definition.
+
+// You have access to the Node class and all the properties on the Linked List class.
+
+// Write tests to prove the following functionality:
+
+// Can successfully add a node to the end of the linked list
+// Can successfully add multiple nodes to the end of a linked list
+// Can successfully insert a node before a node located i the middle of a linked list
+// Can successfully insert a node before the first node of a linked list
+// Can successfully insert after a node in the middle of the linked list
+// Can successfully insert a node after the last node of the linked list
+// Unit tests must be passing before you submit your final solution code.
+
+// Stretch Goal
+// Once you’ve achieved a working solution, write an additional method to delete a node with the given value from the linked list.
