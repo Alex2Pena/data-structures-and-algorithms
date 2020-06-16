fcf4ce714367f5fb7d68d571f6ad7fab29aa733d
Alex Peña <57465115+Alex2Pena@users.noreply.github.com>
2020-06-15 19:54:45 -0700

    Update fizzbuzz-tree.js

--- code-challenges/401/fizzbuzz-tree/fizzbuzz-tree.js
+++ code-challenges/401/fizzbuzz-tree/fizzbuzz-tree.js
@@ -1,24 +1,30 @@
+'use strict';
+let tree = require('../../data-structures/tree/tree.js');
 // Code Challenge
 // Conduct “FizzBuzz” on a tree while traversing through it. Change the values of each of the nodes dependent on the current node’s value
-
-// Specifications
-// Read all of these instructions carefully. Name things exactly as described.
-// Do all your work in a public repository called data-structures-and-algorithms, with a well-formatted, detailed top-level README.md.
-// Create a new branch in your repo called fizzbuzz-tree.
-// Your top-level readme should contain a “Table of Contents” navigation to all of your challenges and implementations so far. (Don’t forget to update it!)
-// This assignment should be completed within the challenges subdirectory of the repository.
-// On your branch, create…
-// C#: a new .NET Core console project named FizzBuzzTree. Within your Program.cs create a new static method outside of Main() following the naming conventions below. Call your newly created method in Main() once complete.
 // JavaScript: a folder named fizzBuzzTree which contains a file called fizz-buzz-tree.js
-// Python: a folder named fizz_buzz_tree which contains a file called fizz_buzz_tree.py
-// Java: a file called FizzBuzzTree.java in your utilities package
-// Include any language-specific configuration files required for this challenge to become an individual component, module, library, etc.
-// NOTE: You can find an example of this configuration for your course in your class lecture repository.
-// Feature Tasks
+
+
+
 // Write a function called FizzBuzzTree which takes a tree as an argument.
 // Without utilizing any of the built-in methods available to your language, determine whether or not the value of each node is divisible by 3, 5 or both. Create a new tree with the same structure as the original, but the values modified as follows:
+const FizzBuzzTree = (root) => {
+  let newVal = '';
 // If the value is divisible by 3, replace the value with “Fizz”
+  if ( root.val % 3 === 0 ) newVal += 'Fizz';
 // If the value is divisible by 5, replace the value with “Buzz”
-// If the value is divisible by 3 and 5, replace the value with “FizzBuzz”
-// If the value is not divisible by 3 or 5, simply turn the number into a String.
+  if ( root.val % 5 === 0 ) newVal += 'Buzz'; 
+
+  let newTreeRoot = new tree.Node( newVal ? newVal : `${root.val}` );
+
+  if ( root.left ) {
+    newTreeRoot.left = FizzBuzzTree(root.left);
+  }
+  if ( root.right ) {
+    newTreeRoot.right = FizzBuzzTree(root.right);
+  }
 // Return the new tree.
+  return newTreeRoot;
+}
+
+module.exports = FizzBuzzTree;
