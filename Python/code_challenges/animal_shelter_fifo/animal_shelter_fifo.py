3a16a38eda2fd27a25c9fed12e9f94bee94c63ac
Alex Peña <attn.alex.p@gmail.com>
2020-11-24 18:45:47 -0800

    completed code challenge

--- Python/code_challenges/animal_shelter_fifo/animal_shelter_fifo.py
+++ Python/code_challenges/animal_shelter_fifo/animal_shelter_fifo.py
@@ -1,77 +1,92 @@
-class Animal :
-    def __init__(self, animal, next_ = None):
+#Node class
+class Animal:
+
+    def __init__(self, animal = None, next_ = None):
         self.animal = animal
-        self.next = next_
+        self.next_ = next_
 
 class InvalidOperationError(Exception):
     pass
 
-class Waiting_list:
+#Stack class
+class Waiting_List:
+
     def __init__(self):
         self.first_up = None
 
     def none_available(self):
-        while self.first_up is None:
-            return True
-        if self.first_up:
-            return False
+        return True if self.first_up else False
 
     def whos_next(self):
-        if self.none_available():
+        if not self.first_up:
             raise InvalidOperationError("All animals have been adopted!")
         return self.first_up.animal
 
     def intake (self, animal):
-        new_animal = Animal(animal)
-        if self.first_up == None:
-            self.first_up = new_animal
-        else:
-            new_animal.next = self.first_up
-            self.first_up = new_animal
+        new_animal = Animal(animal, self.first_up)
+        self.first_up = new_animal
         return self.first_up
 
     def discharge(self):
-        if self.first_up:
-            bye_bye = self.first_up
-            self.first_up = self.first_up.next
-            return bye_bye.animal
+        if not self.first_up:
+            raise InvalidOperationError("All animals have been adopted!")
+        bye_bye = self.first_up
+        self.first_up = self.first_up.next_
+        return bye_bye.animal
 
-class AnimalShelter():
-    def __init__(self):
-        self.shelter = Waiting_list()
-        self.temp_cage = []
+#Queue class
+class Animal_Shelter():
+
+    def __init__(self, waiting_list=None, temp_cage=None):
+        self.shelter = waiting_list if waiting_list else Waiting_List()
+        self.temp_cage = temp_cage if temp_cage else Waiting_List()
+
+    def __str__(self):
+        if not self.shelter.first_up:
+            return None
+        ret_string = f'{self.shelter.first_up.animal}'
+        current = self.shelter.first_up
+        while current.next_:
+            current = current.next_
+            ret_string += f' ->{current.animal}'
+        return ret_string
 
     def welcome_in(self, animal):
         if animal in ['cat','dog']:
-            self.shelter.intake(animal)
-            print(self.shelter.first_up)
+            if not self.shelter.first_up:
+                self.shelter.intake(animal)
+            else:
+                while self.shelter.first_up:
+                    self.temp_cage.intake(self.shelter.discharge())
+                self.temp_cage.intake(animal)
+                while self.temp_cage.first_up:
+                    self.shelter.intake(self.temp_cage.discharge())
         else:
-            print("Sorry we only take dogs & cats here.")
+            raise InvalidOperationError("Sorry we only take dogs & cats here.")
 
-    def adoption_out(self, animal_type):
+    def adoption_out(self, animal_type=None):
+        if not self.shelter.first_up:
+            raise InvalidOperationError("All animals have been adopted!")
         if animal_type in ['cat', 'dog']:
-            while self.shelter.whos_next() != animal_type:
-              self.temp_cage.append(self.shelter.discharge())
+            while self.shelter.first_up:
+                if self.shelter.first_up.animal == animal_type:
+                    break
+                self.temp_cage.intake(self.shelter.discharge())
+            if not self.shelter.first_up:
+                adopted = f'There are no {animal_type}s left.'
+            else:
+                adopted = self.shelter.discharge()
+            while self.temp_cage.first_up:
+                self.shelter.intake(self.temp_cage.discharge())
+        elif animal_type is None:
             adopted = self.shelter.discharge()
-            while self.temp_cage != []:
-                self.shelter.intake(self.temp_cage[-1])
-            print(self.shelter)
-            return adopted
-        if animal_type not in ['cat', 'dog']:
-            return None
+        else:
+            raise InvalidOperationError("Sorry we only keep dogs & cats here.")
+        return adopted
 
 
 if __name__ == "__main__":
-    pass
-
-testrun = AnimalShelter()
-testrun.welcome_in("dog")
-testrun.welcome_in("cat")
-print(testrun.shelter.first_up.animal)
-
-
-
-
-
-
-
+    testrun = Animal_Shelter()
+    testrun.welcome_in("dog")
+    testrun.welcome_in("cat")
+    print(testrun.shelter.first_up.animal)
