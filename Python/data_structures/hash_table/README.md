5f05c76ceea40032d24f3499c491f8ca41f6e248
Alex Peña <attn.alex.p@gmail.com>
2020-12-22 16:05:54 -0800

    completed challenge

--- Python/data_structures/hash_table/README.md
+++ Python/data_structures/hash_table/README.md
@@ -15,7 +15,36 @@ In hashing, large keys are converted into small keys by using hash functions. Th
 > hash: takes in an arbitrary key and returns an index in the collection.
 
 ## Approach & Efficiency
-
+Required is a Node Class, Linked List Class, and Hastable Class
+Included are ADD, CONTAINS, and GET methods for the Hash Table
 
 ## API
-<!-- Description of each method publicly available in each of your hashtable -->
+class Hashtable():
+
+    # adds a new item to the Hashtable
+        # gets a random "index/bucket" number
+        # If there is already something at that index value then make a linked list at that index
+        # append the new node with its key and value
+
+    # Check to see if the table contains a specific key
+        # assigns the hashed version of the original key
+        # if it is found...
+            # returns everything at the index/bucket
+            # loops through the bucket/linkedlist...
+                # If its in the "bucket" return True
+        # else return False
+
+    # returns the value found at a specific key
+        # if we are given a key that exists in the hash table...
+            # assigns the hashed "index/bucket" number
+            # creates a list of all the nodes found in the "index/bucket"
+            # Loops through the bucket and...
+                # returns the value at [1] if the correct value is found
+        # else return an error
+
+    # Creates a random hash number
+        # converts the key to a string
+        # offset by 1
+        # loop through all characters and...
+        # takes the ascii value multiply by 599 then modulo by the size of the table
+        # return the new randomly "hashed" number
--- Python/data_structures/hash_table/hash_table.py
+++ Python/data_structures/hash_table/hash_table.py
@@ -5,33 +5,56 @@ class Hashtable():
         self.size = size
         self.indicies = [None] * self.size
 
+    # adds a new item to the Hashtable
     def add(self, key, val):
+        # gets a random "index/bucket" number
         hashed = self._hash(key)
+        # If there is already something at that index value then make a linked list at that index
         if not self.indicies[hashed]: self.indicies[hashed] = LinkedList()
+        # append the new node with its key and value
         self.indicies[hashed].append_node([key, val])
 
+    # Check to see if the table contains a specific key
     def contains(self, key):
+        # assigns the hashed version of the original key
         hashed_key = self._hash(key)
+        # if it is found...
         if self.indicies[hashed_key]:
+            # returns everything at the index/bucket
             key_list = self.indicies[hashed_key].return_all()
+            # loops through the bucket/linkedlist...
             for vals in key_list:
+                # If its in the "bucket" return True
                 if vals[0] == key: return True
+        # else return False
         return False
 
+    # returns the value found at a specific key
     def get(self, key):
+        # if we are given a key that exists in the hash table...
         if self.contains(key):
+            # assigns the hashed "index/bucket" number
             hashed_key = self._hash(key)
+            # creates a list of all the nodes found in the "index/bucket"
             key_list = self.indicies[hashed_key].return_all()
-            print(key_list)
+            # Loops through the bucket and...
             for values in key_list:
+                # returns the value at [1] if the correct value is found
                 if values[0] == key: return(values[1])
+        # else return an error
         raise ValueError('Key does not exist.')
 
+    # Creates a random number
     def _hash(self, key):
+        # converts the key to a string
         if not isinstance(key, str): key = str(key)
+        # offset by 1
         ascii_val = 1
+        # loop through all characters and...
         for char in key: ascii_val += ord(char)
+        # takes the ascii value multiply by 599 then modulo by the size of the table
         hashed_key = ascii_val * 599 % self.size
+        # return the new randomly "hashed" number
         return hashed_key
 
     def __str__(self):
